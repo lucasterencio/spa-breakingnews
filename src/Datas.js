499bbc848ca1ba7bbdccd1363009540f6054b976
lucasterencio <lucasnn.145@outlook.com>
2025-02-10 10:56:28 -0300

    feat: create data mock

---
+++ src/Datas.js
@@ -0,0 +1,33 @@
+export const news = [
+    {
+        title: "Noticia 1",
+        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates dolore illo, ipsa quod, aspernatur perspiciatis velit, ratione culpa consectetur incidunt mollitia iusto. Odio ab nemo voluptates aliquid ipsa sunt ut.",
+        image: "https://www.oficinadanet.com.br/media/post/25197/330/images.jpg",
+        likes: 2,
+        commentss: 4
+    },
+
+    {
+        title: "Noticia 2",
+        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates dolore illo, ipsa quod, aspernatur perspiciatis velit, ratione culpa consectetur incidunt mollitia iusto. Odio ab nemo voluptates aliquid ipsa sunt ut.",
+        image: "https://www.oficinadanet.com.br/media/post/25197/330/images.jpg",
+        likes: 2,
+        commentss: 4
+    },
+
+    {
+        title: "Noticia 3",
+        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates dolore illo, ipsa quod, aspernatur perspiciatis velit, ratione culpa consectetur incidunt mollitia iusto. Odio ab nemo voluptates aliquid ipsa sunt ut.",
+        image: "https://www.oficinadanet.com.br/media/post/25197/330/images.jpg",
+        likes: 2,
+        commentss: 4
+    },
+
+    {
+        title: "Noticia 4",
+        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates dolore illo, ipsa quod, aspernatur perspiciatis velit, ratione culpa consectetur incidunt mollitia iusto. Odio ab nemo voluptates aliquid ipsa sunt ut.",
+        image: "https://www.oficinadanet.com.br/media/post/25197/330/images.jpg",
+        likes: 2,
+        commentss: 4
+    },
+]
